f06ace2027bd156ec02434e15bb7a2a7a81a020b
Wesley Paulo Ribeiro <wesley_paulo00@hotmail.com>
2026-06-12 17:11:20 -0300

    fix2

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,6 @@
 import type { MetadataRoute } from "next";
 import { siteConfig } from "@/lib/site";
+export const dynamic = 'force-static';
 
 export default function robots(): MetadataRoute.Robots {
   return {
