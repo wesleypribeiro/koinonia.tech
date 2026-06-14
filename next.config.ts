4a2d588e421ec70f39abe16305732ad48cc3480a
Wesley Paulo Ribeiro <wesley_paulo00@hotmail.com>
2026-06-13 21:06:12 -0300

    fix: images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+
+  basePath: '/koinonia.tech'
 };
 
 export default nextConfig;
